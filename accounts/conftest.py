bf79724842c95d621ea4a32a7f2da3b581497fd5
Marcin Dziublewski <poule1@gmail.com>
2025-07-21 22:30:42 +0200

    Add tests for user registration, login, and logout functionality

--- accounts/conftest.py
+++ accounts/conftest.py
@@ -1,1 +1,16 @@
 import pytest
+from django.contrib.auth.models import User
+
+@pytest.fixture
+def users_to_register_success():
+    users = []
+    for i in range(10):
+        username = f'User{i}'
+        password = f'Password123!@#{i}'
+        users.append((username, password))
+    return users
+
+@pytest.fixture
+def user_login_data():
+    user = User.objects.create_user(username='Testusername', password='Password123!@#')
+    return user
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,59 @@
 import pytest
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
-def test_env():
-    assert 1
 
 @pytest.mark.django_db
-def test_env_db():
-    assert 1
+def test_register_user_success(client, users_to_register_success):
+    for username, password in users_to_register_success:
+        data = {
+            'username': username,
+            'password1': password,
+            'password2': password,
+        }
+        response = client.post(reverse('register'), data)
+        assert response.status_code == 302
+        assert User.objects.filter(username=username).exists()
+
+@pytest.mark.django_db
+def test_register_user_fail(client):
+    data = {
+        'username': 'Testusername',
+        'password1': 'Password123!@#',
+        'password2': 'OtherPassword123!@#',
+    }
+    response = client.post(reverse('register'), data)
+    assert response.status_code == 200
+    assert not User.objects.filter(username='Testusername').exists()
+
+@pytest.mark.django_db
+def test_login_user_success(client, user_login_data):
+    data = {
+        'username': 'Testusername',
+        'password': 'Password123!@#',
+    }
+    response = client.post(reverse('login'), data)
+    assert response.status_code == 302
+    assert '_auth_user_id' in client.session
+
+@pytest.mark.django_db
+def test_login_user_fail(client, user_login_data):
+    data = {
+        'username': 'Testusername',
+        'password': 'WrongPassword123!@#',
+    }
+    response = client.post(reverse('login'), data)
+    assert response.status_code == 200
+    assert '_auth_user_id' not in client.session
+
+@pytest.mark.django_db
+def test_logout_user(client, user_login_data):
+    client.login(username=user_login_data.username, password='Password123!@#')
+    assert '_auth_user_id' in client.session
+    data = {
+        'operation': 'Tak'
+    }
+    response = client.post(reverse('logout'), data)
+    assert response.status_code == 302
+    assert '_auth_user_id' not in client.session
