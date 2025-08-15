0bd738d6e70de26715de61277704c7fe2f641dd0
Marcin Dziublewski <poule1@gmail.com>
2025-08-15 16:07:36 +0200

    add CRUD for UserProfile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('register/', views.RegisterUserView.as_view(), name='register'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('create_user_profile/', views.CreateUserProfileView.as_view(), name='create_user_profile'),
+    path('update_user_profile/', views.UpdateUserProfileView.as_view(), name='update_user_profile'),
+    path('delete_user_profile/', views.DeleteUserProfileView.as_view(), name='delete_user_profile'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render,redirect
 from django.views import View
-from .forms import RegisterUserForm, LoginUserForm
+
+from training.forms import CreateWorkoutPlanForm
+from training.models import WorkoutPlan
+from .forms import RegisterUserForm, LoginUserForm, UserProfileForm
+from .models import UserProfile
 
 
 # Create your views here.
@@ -42,3 +47,48 @@ class LogoutUserView(View):
             return redirect('home')
         next_url = request.GET.get('next', 'home')
         return redirect(next_url)
+
+class CreateUserProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        try:
+            user_profile = request.user.userprofile
+            context = {'user_profile': user_profile, 'profile_exists': True}
+        except UserProfile.DoesNotExist:
+            form = UserProfileForm()
+            context = {'form': form, 'profile_exists': False}
+        return render(request, 'accounts/create_user_profile.html', context)
+    def post(self, request):
+        try:
+            if request.user.userprofile:
+                return redirect('create_user_profile')
+        except UserProfile.DoesNotExist:
+            form = UserProfileForm(request.POST)
+            if form.is_valid():
+                user_profile = form.save(commit=False)
+                user_profile.user = request.user
+                user_profile.save()
+                return redirect('create_user_profile')
+            return render(request, 'accounts/create_user_profile.html', {'form': form,'profile_exists': False})
+
+class UpdateUserProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        user_profile = UserProfile.objects.get(user=request.user)
+        form = UserProfileForm(instance=user_profile)
+        return render(request, 'accounts/update_user_profile.html', {'form': form, 'user_profile': user_profile})
+    def post(self, request):
+        user_profile = UserProfile.objects.get(user=request.user)
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('create_user_profile')
+        return render(request, 'accounts/update_user_profile.html', {'form': form, 'user_profile': user_profile})
+
+class DeleteUserProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        user_profile = UserProfile.objects.get(user=request.user)
+        return render(request, 'accounts/delete_user_profile.html', {'user_profile': user_profile})
+    def post(self, request):
+        if request.POST.get('operation') == 'Yes':
+            user_profile = UserProfile.objects.get(user=request.user)
+            user_profile.delete()
+        return redirect('create_user_profile')
